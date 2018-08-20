4c60e0b749a25f0108cf375a258c87d8bbe68982
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2018-08-20 19:58:11 -0300

    fixing bug with item_group migration

--- db/migrate/20171028203324_alter_table_gamificationitem_add_group.rb
+++ db/migrate/20171028203324_alter_table_gamificationitem_add_group.rb
@@ -1,5 +1,6 @@
 class AlterTableGamificationitemAddGroup < ActiveRecord::Migration[5.1]
   def change
-    add_column :gamification_items, :gamification_item_group_id, :integer
+    add_reference :gamification_items, :gamification_item_group,
+      foreign_key: true
   end
 end
